b7cd3d3f64b9439688b8e08e32f20be4dea9d7d1
侯博容 <60761187+houborong@users.noreply.github.com>
2025-01-15 20:06:13 -0300

    Uploading 'exercise-1-8.c'

---
+++ chapter_1/exercise-1-8.c
@@ -0,0 +1,19 @@
+/* Exercise 1-8: Write a program to count blanks, tabs and newlines. */
+
+#include <stdio.h>
+
+int main() {
+  int n, counting;
+
+  counting = 0;
+  while ((n = getchar()) != EOF) {
+    
+    if (n == '\n' || n == '\t' || n == ' ') {
+      ++counting;
+    }
+  }
+  printf("%d\n", counting);
+  return 0;
+}
+
+
